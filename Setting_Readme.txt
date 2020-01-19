87705aa2a19e2dee00f3b53364c7b20d2ccbe922
Sirozha19862k18 <45014354+Sirozha19862k18@users.noreply.github.com>
2020-01-19 19:47:17 +0300

    Initial commit

---
+++ Setting_Readme.txt
@@ -0,0 +1,8 @@
+@UnilineServis_bot
+
+token 
+970419760:AAHqAzpOSxxu-y5r5ZBv8DrC-tJvBbHVSro
+
+ оманды бота:
+
+i_need_help - "начало работы с роботом"
---
+++ src/main/java/Bot.java
@@ -0,0 +1,85 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.swing.text.html.HTML;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            if (update.getMessage().hasText()) {
+                if (update.getMessage().getText().equals("/i_need_help")) {
+                    try {
+                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else if (update.hasCallbackQuery()) { //обработка нажатий кнопок
+            processCallbackQuery(update);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "UnilineServis_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "970419760:AAHqAzpOSxxu-y5r5ZBv8DrC-tJvBbHVSro";
+    }
+
+    private void processCallbackQuery(Update update) { //метод обработки нажатий кнопок от CallBaqQuere
+        String pointOfchangeUser = update.getCallbackQuery().getData();
+
+        try {
+            execute(new SendMessage().setText(CommandList.MessageList(pointOfchangeUser)).setParseMode("HTML")
+                    .setChatId(update.getCallbackQuery().getMessage().getChatId()));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println(pointOfchangeUser);
+    }
+
+    public static SendMessage sendInlineKeyBoardMessage ( long chatId){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton4 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton5 = new InlineKeyboardButton();
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow4 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow5 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        keyboardButtonsRow3.add(inlineKeyboardButton3);
+        keyboardButtonsRow4.add(inlineKeyboardButton4);
+        keyboardButtonsRow5.add(inlineKeyboardButton5);
+        inlineKeyboardButton1.setText("1. Узнать контакты сервиса").setCallbackData("Contacts");
+        inlineKeyboardButton2.setText("2. Повышенная остаточная масличность").setCallbackData("HightOil");
+        inlineKeyboardButton3.setText("3. Экструдер стреляет").setCallbackData("ExtruderBOOM");
+        inlineKeyboardButton4.setText("4. Пресс не давит масло").setCallbackData("PressNotOil");
+        inlineKeyboardButton5.setText("5. Фильтр не закрывается\\открывается").setCallbackData("FilterNotOpen");
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        rowList.add(keyboardButtonsRow3);
+        rowList.add(keyboardButtonsRow4);
+        rowList.add(keyboardButtonsRow5);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Варианты действий:").setReplyMarkup(inlineKeyboardMarkup);
+    }
+}
+
